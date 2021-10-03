786e4580e28cc6bd3731dc2f3eaf103e42c21383
Mahdi Sahebi <msahebi@gmail.com>
2021-10-03 13:56:35 +0330

    .

--- FSRCNN/model.py
+++ FSRCNN/model.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
 
-class Net(torch.nn.Module):
-    def __init__(self, num_channels, upscale_factor=1, d=64, s=12, m=4):
-        super(Net, self).__init__()
-        self.upscale_factor = 1
 
+class Net(torch.nn.Module):
+    def __init__(self, num_channels, upscale_factor, d=64, s=12, m=4):
+        super(Net, self).__init__()
+
+        self.upscale_factor=1;
         self.first_part = nn.Sequential(nn.Conv2d(in_channels=num_channels, out_channels=d, kernel_size=5, stride=1, padding=2),
                                         nn.PReLU())
 
@@ -20,16 +21,13 @@ class Net(torch.nn.Module):
 
         self.mid_part = torch.nn.Sequential(*self.layers)
 
-        self.last_part = nn.ConvTranspose2d(in_channels=d, out_channels=2, kernel_size=9, stride=upscale_factor, padding=3, output_padding=1)
-
-        self.end        = nn.Sequential(nn.Conv2d(in_channels=2, out_channels=d, kernel_size=5, stride=1, padding=2),
-                                        nn.PReLU())
+        # Deconvolution
+        self.last_part = nn.ConvTranspose2d(in_channels=d, out_channels=num_channels, kernel_size=9, stride=upscale_factor, padding=3, output_padding=1)
 
     def forward(self, x):
         out = self.first_part(x)
         out = self.mid_part(out)
         out = self.last_part(out)
-        out = self.end(out)
         return out
 
     def weight_init(self, mean=0.0, std=0.02):
